34d89e0688b9a5f429b8666d546e631e42572ad0
luwukien <luukien2910@gmail.com>
2025-02-14 23:44:23 +0700

    feat: new update

--- public/main.js
+++ public/main.js
@@ -6,8 +6,13 @@ const toggleTopMenuIcon = document.getElementById('ct-toggle-top-menu-icon');
 document.addEventListener('click', (e) => {
   if (toggleTopMenuIcon.contains(e.target)) {
     //Click on toggleTopMenuIcon
+    topMenu.classList.toggle('ct-top-menu-expanded');
     topMenu.classList.toggle('hidden'); 
   } else {
     //Click outside toggleTopMenuIcon
+    if (topMenu.classList.contains('ct-top-menu-expanded')) {
+      topMenu.classList.remove('ct-top-menu-expanded');
+      topMenu.classList.add('hidden');
+    }
   }
 });
